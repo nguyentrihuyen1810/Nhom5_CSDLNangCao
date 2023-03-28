7b35fd68b32dd1012cc06a8f35cff86bb7eb1c1d
ngothimylanh <117974278+ngothimylanh@users.noreply.github.com>
2023-03-28 11:21:08 +0700

    DANG XUAT

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customers;
+use App\Models\Logout;
+use Illuminate\Http\Request;
+use Auth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function create ()
+    {
+        return view('/logout');
+    }
+
+    public function checklogout(){
+        Auth::logout();
+        return redirect('/home');
+    }
+
+
+    }
+
+   
+    
